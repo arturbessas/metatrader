c9430f69d1dab81d96e4c39d6a59808cb612fab9
Artur Bessas <artur.bessas@smarttbotter.com>
2021-04-15 19:40:24 -0300

    Update novo

--- MediaDX.c
+++ MediaDX.c
@@ -12,6 +12,7 @@
 #include <Trade\OrderInfo.mqh>
 
 #define MAX_TRIES 5
+#define TIMEOUT 30
 
 //--- input parameters
 input int tipoMedia;
@@ -23,6 +24,16 @@ input double stopLoss;
 input double takeProfit;
 input string increasesQttStr = "";
 input string increasesPtsStr = "";
+input int firstIncreasePts = 0;
+input int firstIncreaseStocks = 0;
+input int secondIncreasePts = 0;
+input int secondIncreaseStocks = 0;
+input int thirdIncreasePts = 0;
+input int thirdIncreaseStocks = 0;
+input int fourthIncreasePts = 0;
+input int fourthIncreaseStocks = 0;
+input int fifthIncreasePts = 0;
+input int fifthIncreaseStocks = 0;
 
 int mediaHandle;
 double media;
@@ -47,18 +58,25 @@ int increasesQtt[];
 int i, j, k;
 double entryPrice = 0.0;
 bool validStrategy = true;
+ulong entryOrder = 0;
+bool lockEntries = false;
+datetime entryTime;
+
 //+------------------------------------------------------------------+
 //| Expert initialization function                                   |
 //+------------------------------------------------------------------+
 int OnInit()
 {
+	trade.SetTypeFilling(ORDER_FILLING_FOK);
 	ArraySetAsSeries(aux, true);
 	mediaHandle = iMA(_Symbol, getPeriodoGrafico(), periodoMedia, 0, getTipoMedia(), PRICE_CLOSE);
 	stock = Symbol();
 	validStrategy = processIncreases();
+	if (validStrategy && increasesPtsStr == "")
+		validStrategy = reprocessIncreases();
 
 	if (!validStrategy)
-		Print("falhouuuu");
+		ExpertRemove();
 
 	return (INIT_SUCCEEDED);
 }
@@ -75,11 +93,8 @@ void OnTick()
 	getMedia();
 
 	//regra de entrada
-	if (!posManager.Select(stock))
+	if (!posManager.Select(stock) && !isEntryLocked())
 		getEntry();
-
-	//send stop gain
-	sendTP();
 }
 
 void OnTradeTransaction(const MqlTradeTransaction &trans,
@@ -90,6 +105,13 @@ void OnTradeTransaction(const MqlTradeTransaction &trans,
 	{
 		if (trans.order == lastIncreaseOrder && trans.order_state == ORDER_STATE_FILLED)
 		{
+			if (increaseStep == 0)
+			{
+				entryPrice = posManager.PriceOpen();
+				//send stop gain
+				sendTP();
+				lockEntries = false;
+			}
 			//send new increase
 			if (increaseStep < increaseNumber)
 			{
@@ -141,27 +163,44 @@ void getEntry()
 		tpSent = false;
 		increaseStep = 0;
 		int attempts = 0;
+		entryPrice = 0;
 		do
 		{
 			attempts++;
 			if ((trendwise && getSignal() == "upper") || (!trendwise && getSignal() == "lower"))
 			{
-				trade.Buy(1.0, stock, 0.0, 0.0, 0.0, "Compra por rompimento de banda");
+				trade.BuyLimit(1.0, tick.last, stock, 0.0, 0.0, ORDER_TIME_GTC, 0, "Compra por rompimento de banda");
 				opType = "buy";
 			}
 			else if ((trendwise && getSignal() == "lower") || (!trendwise && getSignal() == "upper"))
 			{
-				trade.Sell(1.0, stock, 0.0, 0.0, 0.0, "Venda por rompimento de banda");
+				trade.SellLimit(1.0, tick.last, stock, 0.0, 0.0, ORDER_TIME_GTC, 0, "Venda por rompimento de banda");
 				opType = "sell";
 			}
 		} while (trade.ResultRetcode() != 10009 && attempts <= MAX_TRIES);
 
-		if (posManager.Select(stock))
+		if (attempts <= MAX_TRIES)
 		{
-			entryPrice = posManager.PriceOpen();
+			entryTime = tick.time;
+			lockEntries = true;
 			lastIncreaseOrder = trade.ResultOrder();
 		}
 	}
+}
+
+bool isEntryLocked()
+{
+	if (lockEntries)
+	{
+		if (tick.time - entryTime > TIMEOUT)
+		{
+			lockEntries = false;
+			trade.OrderDelete(lastIncreaseOrder);
+			lastIncreaseOrder = 0;
+		}
+	}
+
+	return lockEntries;
 }
 
 string getSignal()
@@ -194,7 +233,12 @@ bool validTick()
 	TimeToStruct(tick.time, tempo);
 
 	if (tempo.hour < 9)
+	{
+		increaseStep = 0;
+		lastIncreaseOrder = 0;
+		tpOrder = 0;
 		return false;
+	}
 
 	if (!isPositioned && order.Select(tpOrder))
 	{
@@ -202,7 +246,7 @@ bool validTick()
 			cancelStopGain();
 	}
 
-	if (!isPositioned && order.Select(lastIncreaseOrder))
+	if (!isPositioned && order.Select(lastIncreaseOrder) && increaseStep)
 	{
 		if (order.State() != ORDER_STATE_FILLED && order.State() != ORDER_STATE_CANCELED)
 			cancelIncreaseOrder();
@@ -215,6 +259,7 @@ bool validTick()
 	{
 		trade.PositionClose(stock);
 		cancelStopGain();
+		cancelIncreaseOrder();
 		return false;
 	}
 
@@ -243,9 +288,48 @@ void cancelIncreaseOrder()
 	lastIncreaseOrder = 0;
 }
 
+bool reprocessIncreases()
+{
+	if (firstIncreasePts && firstIncreaseStocks)
+	{
+		add(increasesPts, firstIncreasePts);
+		add(increasesQtt, firstIncreaseStocks);
+		if (secondIncreasePts && secondIncreaseStocks)
+		{
+			add(increasesPts, secondIncreasePts);
+			add(increasesQtt, secondIncreaseStocks);
+			if (thirdIncreasePts && thirdIncreaseStocks)
+			{
+				add(increasesPts, thirdIncreasePts);
+				add(increasesQtt, thirdIncreaseStocks);
+				if (fourthIncreasePts && fourthIncreaseStocks)
+				{
+					add(increasesPts, fourthIncreasePts);
+					add(increasesQtt, fourthIncreaseStocks);
+					if (fifthIncreasePts && fifthIncreaseStocks)
+					{
+						add(increasesPts, fifthIncreasePts);
+						add(increasesQtt, fifthIncreaseStocks);
+					}
+				}
+			}
+		}
+	}
+
+	increaseNumber = ArraySize(increasesPts);
+
+	for (i = 1; i < increaseNumber; i++)
+	{
+		if (increasesPts[i] <= increasesPts[i - 1])
+			return false;
+	}
+
+	return true;
+}
+
 bool processIncreases()
 {
-	if (increasesPtsStr == "" || increasesQttStr == "")
+	if (increasesPtsStr == "" && increasesQttStr == "")
 		return true;
 
 	string auxStr[];
